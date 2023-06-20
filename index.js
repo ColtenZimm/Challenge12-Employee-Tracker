d2ffe74941fba1b68fdb3162c109e47518158e23
ColtenZimm <coltenz00@gmail.com>
2023-06-20 15:30:56 -0500

    added display by manager

--- index.js
+++ index.js
@@ -171,4 +171,25 @@ const mainMenu = () => {
         });
       }
     }
-    
+    function ViewAllEmployeesByManager() {
+        // display a list includes all managers: first name, last name
+        const query = `SELECT 
+         employee.id, 
+         employee.first_name, 
+         employee.last_name, 
+         role.title, 
+         department.name AS 
+         department, 
+         CONCAT(manager.first_name, ' ', manager.last_name) AS manager 
+         FROM employee 
+         LEFT JOIN role ON employee.role_id = role.id 
+         LEFT JOIN department ON role.department_id = department.id 
+         LEFT JOIN employee manager ON manager.id = employee.manager_id 
+         ORDER BY manager;`;
+        connection.query(query, (err, data) => {
+          if (err) throw err;
+          console.table(data);
+          mainMenu();
+        });
+      }
+      
