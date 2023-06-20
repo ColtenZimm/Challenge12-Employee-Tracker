c506467aad58046688ee22e99b3ba5f0e7c1fd24
ColtenZimm <coltenz00@gmail.com>
2023-06-20 15:26:07 -0500

    added employee roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,14 @@ VALUES  ('Marketing'),
         ('Engineering'), 
         ('Human Resources'),
         ('Legal');
+
+        INSERT INTO role (title, salary, department_id) 
+VALUES  ('Marketing Manager', 100000, 1),
+        ('Marketing Analyst', 55000, 1),
+        ('Accounting Manager', 120000, 2),
+        ('Accountant', 70000, 2),
+        ('Tech Lead', 150000, 3),
+        ('Software Engineer', 80000, 3),
+        ('HR Manager', 110000, 4),
+        ('Recruiter', 50000, 4),
+        ('Corporate Lawyer', 130000, 5);
